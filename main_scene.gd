7592f57c81a388ab432eb5f58d60aac7cc94e0b1
RubbishCans <tomhofverberg@gmail.com>
2024-11-18 09:31:52 +0100

    tenth commit

--- main_scene.gd
+++ main_scene.gd
@@ -8,7 +8,6 @@ var visible_text_tween;
 var enemy = preload("res://characters/enemy.tscn")
 
 func _ready() -> void:
-	timer.timeout.connect(_on_timer_timeout)
 	tile_map.display_sign_text.connect(display_sign_text)
 	tile_map.hide_sign_text.connect(hide_sign_text)
 
@@ -72,9 +71,9 @@ func enemy_random_position():
 		return enemy_random_position()
 	elif random_position.x >= 204 and random_position.x < 234 and random_position.y >= 190 and random_position.y < 218: #medium stone
 		return enemy_random_position()
-	elif random_position.x >= 236 and random_position.x < 238 and random_position.y >= 238 and random_position.y < 268: #bottom medium bush
+	elif random_position.x >= 238 and random_position.x < 270 and random_position.y >= 242 and random_position.y < 268: #bottom medium bush
 		return enemy_random_position()
-	elif random_position.x >= 292 and random_position.x < 274 and random_position.y >= 274 and random_position.y < 296: #bottom mini stone
+	elif random_position.x >= 292 and random_position.x < 322 and random_position.y >= 274 and random_position.y < 298: #bottom mini stone
 		return enemy_random_position()
 	else:
 		return random_position
